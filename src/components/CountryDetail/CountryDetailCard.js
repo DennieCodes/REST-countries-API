67348642f800c2bfbb132c5e45a9e815bbed9b0e
Oculareo <oculareo@gmail.com>
2021-10-03 08:38:04 -0400

    Refactored Country Detail Card functions into separate components

--- src/components/CountryDetail/CountryDetailCard.js
+++ src/components/CountryDetail/CountryDetailCard.js
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import styled from "styled-components";
 
-import CountryDetailBorderSection from "./CountryDetailBorderSection";
+import CountryDetailStats from "./CountryDetailStats";
 import { CountryDataContext } from "../../contexts/CountryDataContext";
 
 const CountryDetails = styled.div`
+  display: flex;
+  flex-direction: column;
   margin: 0 1.5rem;
 `;
 
@@ -12,26 +14,6 @@ const Flag = styled.img`
   height: 14.25rem;
   width: 20rem;
   object-fit: cover;
-`;
-
-const CountryDetailsInfo = styled.div`
-  margin: 3rem 0;
-  font-family: ${(props) => props.theme.fonts.main};
-`;
-
-const CountryDetailsSubInfo = styled.div`
-  margin-top: 1.54rem;
-`;
-
-const CountryName = styled.h2`
-  margin-bottom: 2rem;
-  font-size: 1.25rem;
-  font-weight: 800;
-`;
-
-const CountryStat = styled.p`
-  font-size: 0.875rem;
-  line-height: 2.2rem;
 `;
 
 // CountryDetailCard Component
@@ -45,22 +27,7 @@ const CountryDetailCard = (props) => {
     return data.name === countryName;
   });
 
-  const {
-    name,
-    flags,
-    population,
-    region,
-    capital,
-    subregion,
-    currencies,
-    languages,
-    topLevelDomain,
-    borders,
-  } = countryDetails[0];
-
-  // Parse name from currencies and languages which are array of objects and then join with comma separator
-  const countryCurrencies = currencies.map((data) => data.name).join(", ");
-  const countryLanguages = languages.map((data) => data.name).join(", ");
+  const { name, flags } = countryDetails[0];
 
   const flagImageAlternateText = `The flag of ${name}`;
 
@@ -68,37 +35,7 @@ const CountryDetailCard = (props) => {
     <CountryDetails>
       <Flag src={flags.svg} alt={flagImageAlternateText} />
 
-      <CountryDetailsInfo>
-        <CountryName>{name}</CountryName>
-        <CountryStat>
-          <strong>Population:</strong>{" "}
-          {population.toLocaleString("en", { useGrouping: true })}
-        </CountryStat>
-        <CountryStat>
-          <strong>Region:</strong> {region}
-        </CountryStat>
-        <CountryStat>
-          <strong>Subregion:</strong> {subregion}
-        </CountryStat>
-        <CountryStat>
-          <strong>Capital:</strong> {capital}
-        </CountryStat>
-        <CountryDetailsSubInfo>
-          <CountryStat>
-            <strong>Top level Domain:</strong> {topLevelDomain[0]}
-          </CountryStat>
-
-          <CountryStat>
-            <strong>Currencies:</strong> {countryCurrencies}
-          </CountryStat>
-
-          <CountryStat>
-            <strong>Languages:</strong> {countryLanguages}
-          </CountryStat>
-        </CountryDetailsSubInfo>
-
-        <CountryDetailBorderSection borders={borders} />
-      </CountryDetailsInfo>
+      <CountryDetailStats countryDetail={countryDetails[0]} />
     </CountryDetails>
   );
 };
---
+++ src/components/CountryDetail/CountryDetailStats.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import CountryDetailBorderSection from "./CountryDetailBorderSection";
+
+const CountryDetailsInfo = styled.div`
+  margin: 3rem 0;
+  font-family: ${(props) => props.theme.fonts.main};
+`;
+
+const CountryName = styled.h2`
+  margin-bottom: 2rem;
+  font-size: 1.25rem;
+  font-weight: 800;
+`;
+
+const CountryStat = styled.p`
+  font-size: 0.875rem;
+  line-height: 2.2rem;
+`;
+
+const CountryDetailsSubInfo = styled.div`
+  margin-top: 1.54rem;
+`;
+
+// CountryDetailStats Component
+const CountryDetailStats = (props) => {
+  const {
+    name,
+    borders,
+    population,
+    region,
+    capital,
+    subregion,
+    currencies,
+    languages,
+    topLevelDomain,
+  } = props.countryDetail;
+
+  const countryCurrencies = currencies.map((data) => data.name).join(", ");
+  const countryLanguages = languages.map((data) => data.name).join(", ");
+
+  return (
+    <CountryDetailsInfo>
+      <CountryName>{name}</CountryName>
+      <CountryStat>
+        <strong>Population:</strong>{" "}
+        {population.toLocaleString("en", { useGrouping: true })}
+      </CountryStat>
+      <CountryStat>
+        <strong>Region:</strong> {region}
+      </CountryStat>
+      <CountryStat>
+        <strong>Subregion:</strong> {subregion}
+      </CountryStat>
+      <CountryStat>
+        <strong>Capital:</strong> {capital}
+      </CountryStat>
+
+      <CountryDetailsSubInfo>
+        <CountryStat>
+          <strong>Top level Domain:</strong> {topLevelDomain[0]}
+        </CountryStat>
+
+        <CountryStat>
+          <strong>Currencies:</strong> {countryCurrencies}
+        </CountryStat>
+
+        <CountryStat>
+          <strong>Languages:</strong> {countryLanguages}
+        </CountryStat>
+      </CountryDetailsSubInfo>
+
+      <CountryDetailBorderSection borders={borders} />
+    </CountryDetailsInfo>
+  );
+};
+
+export default CountryDetailStats;
